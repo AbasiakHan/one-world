b85e59a96cf33fdf5e699c5ab40c58053c7af77c
abasiakhan <abas.hanson@gmail.com>
2020-06-10 09:04:55 +0100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+/*
+var makeNoise;
+
+makeNoise = function(){
+  return 'Pling!'
+}
+
+console.log(makeNoise());
+
+var result = 1;
+
+var power = function (base, exponent) {
+  for(var count = 0; )
+}
+*/
